37b1afaf7ad4e2fb9d48b096deb33e359f704b0d
Dimitris Stavroulakis <jimstav@gmail.com>
2024-11-24 19:25:30 +0200

    Loading and Showing SVGs

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,26 @@
+import bird from "./resources/svg/bird.svg";
+import cat from "./resources/svg/cat.svg";
+import cow from "./resources/svg/cow.svg";
+import dog from "./resources/svg/dog.svg";
+import gator from "./resources/svg/gator.svg";
+import heart from "./resources/svg/heart.svg";
+import horse from "./resources/svg/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
 function AnimalShow({ type }) {
-  return <div>{type}</div>;
+  return (
+    <div>
+      <img src={svgMap[type]} alt={type} />
+    </div>
+  );
 }
 
 export default AnimalShow;
